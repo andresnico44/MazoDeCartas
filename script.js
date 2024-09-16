4cfc7ff8229113660b3b4c3adcf5e4d92e073094
andresnico44 <ramiroandresnc@ufps.edu.co>
2024-09-15 23:11:58 -0500

    Correcciones en las redirecciones del login al iniciar sesión

--- script.js
+++ script.js
@@ -1,68 +1,73 @@
-document.querySelector('#registrar').addEventListener('click', function() { 
-	guardarCarta();
-	pintarTabla();
+document.querySelector('#enviar').addEventListener('click', function() { 
+    // Validación simple del formulario
+    var username = document.querySelector('#username').value;
+    var password = document.querySelector('#password').value;
+
+    if (username && password) {
+        // Redirigir a cartas.html
+        window.location.href = 'cartas.html';
+    } else {
+        // Mostrar alerta si los campos están vacíos
+        document.querySelector('#alerta').style.display = 'block';
+        document.querySelector('#alerta').textContent = 'Please fill in all fields.';
+    }
 });
 
+document.querySelector('#registrar').addEventListener('click', function() { 
+    guardarCarta();
+    pintarTabla();
+});
 
 $(".btncarta").click(function(){
-  var datos = localStorage.getItem('datos');
+    var datos = localStorage.getItem('datos');
 
-	datos = JSON.parse(datos);
+    datos = JSON.parse(datos);
 
-	for(let item of datos) {
-		if(item.numero == this.dataset.carta) {
-			item.cantidad ++;
-		}
-	}
+    for(let item of datos) {
+        if(item.numero == this.dataset.carta) {
+            item.cantidad++;
+        }
+    }
 
-	localStorage.setItem('datos', JSON.stringify(datos));
-	pintarTabla();
+    localStorage.setItem('datos', JSON.stringify(datos));
+    pintarTabla();
 });
 
 function guardarCarta(){
+    var numero = document.querySelector('#numero').value;
+    var carta = document.querySelector('#carta').value;
+    var datos = localStorage.getItem('datos');
 
-	var numero = document.querySelector('#numero').value;
-	var carta = document.querySelector('#carta').value;
-	var datos = localStorage.getItem('datos');
+    datos = JSON.parse(datos);
 
-	datos = JSON.parse(datos);
+    var dato = {numero: numero, carta: carta, cantidad: '0'};
 
-	var dato = {numero: numero, carta: carta, cantidad: '0'};
+    datos.push(dato);
 
-	datos.push(dato);
-
-	localStorage.setItem('datos', JSON.stringify(datos));
+    localStorage.setItem('datos', JSON.stringify(datos));
 }
 
 function cargarJSON(){
-	var miObjeto = [
-				{ 'numero': '1', 'carta': 'as', 'cantidad': '2' },
-				{ 'numero': '2', 'carta': '2 de diamantes', 'cantidad': '3' }];
-		localStorage.setItem('datos', JSON.stringify(miObjeto));
+    var miObjeto = [
+                { 'numero': '1', 'carta': 'as', 'cantidad': '2' },
+                { 'numero': '2', 'carta': '2 de diamantes', 'cantidad': '3' }];
+    localStorage.setItem('datos', JSON.stringify(miObjeto));
 }
 
 function pintarTabla(){
-	var datos = localStorage.getItem('datos');
+    var datos = localStorage.getItem('datos');
 
-	let res = document.querySelector('#listado');
-		res.innerHTML = '';
+    let res = document.querySelector('#listado');
+    res.innerHTML = '';
 
-	console.log('objetoObtenido: ', JSON.parse(datos));
-	datos = JSON.parse(datos);
+    console.log('objetoObtenido: ', JSON.parse(datos));
+    datos = JSON.parse(datos);
 
-	for(let item of datos) {
-		res.innerHTML += `<tr>
-  				<td>${item.numero}</td>
-  				<td>${item.carta}</td>
-  				<td>${item.cantidad}</td>
-			</tr>`;
-	}
+    for(let item of datos) {
+        res.innerHTML += `<tr>
+                <td>${item.numero}</td>
+                <td>${item.carta}</td>
+                <td>${item.cantidad}</td>
+            </tr>`;
+    }
 }
-function leerJSON() {
-            $.getJSON("", function(datos) {
-                console.log(datos);
-            });
-        }
-leerJSON();
-cargarJSON();
-pintarTabla();
